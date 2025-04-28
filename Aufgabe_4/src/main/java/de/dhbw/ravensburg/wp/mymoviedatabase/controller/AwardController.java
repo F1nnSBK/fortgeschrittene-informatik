67388bc5b85b389aa33ac9aad2e44de384bbba5f
Finn <fhertsch235@gmail.com>
2025-04-28 10:00:31 +0200

    AwardController Interface

--- Aufgabe_4/src/main/java/de/dhbw/ravensburg/wp/mymoviedatabase/controller/AwardController.java
+++ Aufgabe_4/src/main/java/de/dhbw/ravensburg/wp/mymoviedatabase/controller/AwardController.java
@@ -1,4 +1,26 @@
 package de.dhbw.ravensburg.wp.mymoviedatabase.controller;
 
+import de.dhbw.ravensburg.wp.mymoviedatabase.model.Award;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public interface AwardController {
+    @GetMapping
+    List<Award> getAllAwards();
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Award addAward(@RequestBody Award award);
+
+    @GetMapping("/{awardId}")
+    Award getAwardById(@PathVariable("awardId") Long awardId);
+
+    @PutMapping("/{awardId}")
+    Award updateAward(@PathVariable("awardId") Long awardId, @RequestBody Award award);
+
+    @DeleteMapping("/{awardId}")
+    void deleteAwardById(@PathVariable("awardId") Long awardId);
+
 }
--- Aufgabe_4/src/main/java/de/dhbw/ravensburg/wp/mymoviedatabase/controller/AwardControllerImpl.java
+++ Aufgabe_4/src/main/java/de/dhbw/ravensburg/wp/mymoviedatabase/controller/AwardControllerImpl.java
@@ -1,4 +1,48 @@
 package de.dhbw.ravensburg.wp.mymoviedatabase.controller;
 
+import de.dhbw.ravensburg.wp.mymoviedatabase.model.Award;
+import de.dhbw.ravensburg.wp.mymoviedatabase.service.AwardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/awards")
 public class AwardControllerImpl {
+
+    AwardService awardService;
+
+    @Autowired
+    public AwardControllerImpl(AwardService awardService) {
+        this.awardService = awardService;
+    }
+
+    @GetMapping
+    public List<Award> getAllAwards() { return this.awardService.getAllAwards(); }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Award addAward(@RequestBody Award award) {
+        return this.awardService.addAward(award);
+    }
+
+    @GetMapping("/{awardId}")
+    public Award getAwardById(@PathVariable("awardId") Long awardId) {
+        return this.awardService.getAwardById(awardId);
+    }
+
+    @PutMapping("/{awardId}")
+    public Award updateAward(@PathVariable("awardId") Long awardId, @RequestBody Award award) {
+        award.setId(awardId);
+        return this.awardService.updateAward(award);
+    }
+
+    @DeleteMapping("/{awardId}")
+    public void deleteAwardById(@PathVariable("awardId") Long awardId) {
+        this.awardService.removeAwardById(awardId);
+    }
+
+
 }
